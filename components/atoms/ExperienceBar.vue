bc00473c1b10f9f3a26b031d627b4adfca2f6ae8
Fernando Zanchetta <fernandozanca94@gmail.com>
2021-03-27 02:30:10 -0300

    Create atoms CompletedChallenges and ExperienceBar, and molecule Profile

---
+++ components/atoms/ExperienceBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="pb-10 flex items-center">
+    <p class="text-sm">
+      {{ `${xp.start} xp` }}
+    </p>
+    <div class="mx-4 lg:mx-6 flex-1 bg-gray-line h-1 rounded-md">
+      <div :style="{ width: `${currentXpPercentage}%` }" class="relative bg-green h-1">
+        <p class="text-sm absolute w-max top-2">
+          {{ `${xp.current} xp (${currentXpPercentage}%)` }}
+        </p>
+      </div>
+    </div>
+    <p class="text-sm">
+      {{ `${xp.end} xp` }}
+    </p>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapState, mapGetters } from 'vuex'
+
+export default Vue.extend({
+  computed: {
+    ...mapState('Challenges', ['xp']),
+    ...mapGetters('Challenges', ['currentXpPercentage'])
+  }
+})
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    teste
-  </div>
+  <section class="flex flex-col lg:flex-row flex-1 lg:flex-none lg:mt-16 sm:gap-x-10 md:gap-x-20">
+    <div class="flex- flex-col w-full lg:w-1/2">
+      <Profile />
+      <CompletedChallenges />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue'
+import Profile from '~/components/molecules/Profile.vue'
+
+interface Head {
+  title: string
+}
+
+export default Vue.extend({
+  head (): Head {
+    return {
+      title: 'Home | pomo-gamification'
+    }
+  },
+  components: {
+    CompletedChallenges,
+    Profile
+  }
+})
 </script>
